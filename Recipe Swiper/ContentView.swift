537ca5933f629a8d6dfc230f91bed87a0bbc38b4
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-04-11 11:17:30 -0500

    Initial Commit

---
+++ Recipe Swiper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe Swiper
+//
+//  Created by Raphael Abano on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe Swiper/Recipe_SwiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_SwiperApp.swift
+//  Recipe Swiper
+//
+//  Created by Raphael Abano on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_SwiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
