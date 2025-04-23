5d4efe9efa3c5cc13ed4954719d4f1ecef9bb05d
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-04-22 23:59:25 -0500

    In Progress SettingsView

---
+++ Recipe Swiper/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Recipe Swiper
+//
+//  Created by Raphael Abano on 4/22/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
