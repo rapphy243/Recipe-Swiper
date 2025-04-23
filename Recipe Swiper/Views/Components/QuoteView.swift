131a7dc44ae38d17ab75564c1235c7b9269f958a
tylerberlin <berlity0611@gmail.com>
2025-04-23 09:46:17 -0500

    Added inspirational quotes

---
+++ Recipe Swiper/Views/Components/QuoteView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteView.swift
+//  Recipe Swiper
+//
+//  Created by Tyler Berlin on 4/23/25.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteView()
+}
