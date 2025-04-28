9e715f7cd9abdc45a8a0bcfa208a9c6da85fab60
Zane <zmatarieh@bsd220.org>
2025-04-28 10:04:45 -0500

    added new view & integrrated just have to get the page to look good

---
+++ Recipe Swiper/Views/OnboardingPages/OnboardingFiltersView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingFiltersView.swift
+//  Recipe Swiper
+//
+//  Created by Zane Matarieh on 4/28/25.
+//
+
+import SwiftUI
+
+struct OnboardingFiltersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingFiltersView()
+}
