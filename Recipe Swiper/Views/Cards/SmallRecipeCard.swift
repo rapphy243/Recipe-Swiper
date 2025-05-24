ef62063f71a88033fadc1d2f5420472cbc742f5e
Rapphy <36800640+rapphy243@users.noreply.github.com>
2025-05-24 15:13:00 -0500

    Improve SmallRecipeCard

--- Recipe Swiper/Views/Cards/SmallRecipeCard.swift
+++ Recipe Swiper/Views/Cards/SmallRecipeCard.swift
@@ -26,13 +26,13 @@ struct SmallRecipeCard: View {
                             image
                                 .resizable()
                                 .scaledToFill()
-                                .frame(width: geometry.size.width * 0.5, height: 260)
+                                .frame(width: geometry.size.width * 0.5, height: 300)
                                 .clipped()
                                 .cornerRadius(10)
                         } placeholder: {
                             RoundedRectangle(cornerRadius: 10)
                                 .fill(Color.gray.opacity(0.3))
-                                .frame(width: geometry.size.width * 0.5, height: 260)
+                                .frame(width: geometry.size.width * 0.5, height: 300)
                         }
                         VStack {
                             SmallCardDetails(recipe: recipe)
@@ -47,8 +47,9 @@ struct SmallRecipeCard: View {
                         }
                     }
                 }
-                .frame(height: 260)
+                .padding(.top)
             }
+            .frame(height: 400)
             .padding(.vertical)
         }
     }
