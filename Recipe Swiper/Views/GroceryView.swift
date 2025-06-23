f93ba42e03af8e8a3934ccbf89a13d72e80d924d
Rapphy <36800640+rapphy243@users.noreply.github.com>
2025-06-22 21:51:31 -0500

    Start Rewrite

--- Recipe Swiper/Views/GroceryView.swift
+++ Recipe Swiper/Views/GroceryView.swift
@@ -1,5 +1,5 @@
 //
-//  OnboardingView.swift
+//  GroceryView.swift
 //  Recipe Swiper
 //
 //  Created by Raphael Abano on 6/22/25.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct OnboardingView: View {
+struct GroceryView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    OnboardingView()
+    GroceryView()
 }
