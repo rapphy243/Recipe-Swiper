ce48e25e3baaa162e0e112517b6fdb7d336ccdae
Rapphy <36800640+rapphy243@users.noreply.github.com>
2025-07-03 22:25:20 -0500

    Move SwipableRecipeCard and add ability for save/discard

--- Recipe Swiper/Views/MainView/MainView.swift
+++ Recipe Swiper/Views/MainView/MainView.swift
@@ -5,7 +5,6 @@
 //  Created by Raphael Abano on 6/22/25.
 //
 
-
 import SwiftUI
 
 // Implemented MVVM?
@@ -29,13 +28,33 @@ class MainViewModel: ObservableObject {
         self.showSettings = false
         self.recipe = recipe
     }
+
+    func saveRecipe() async {
+        let savedRecipe = RecipeModel(from: self.recipe)
+        print("Saved: \(savedRecipe.title)")
+        await fetchNewRecipe()
+
+    }
+    func discardRecipe() async {
+        print("Discarded: \(self.recipe.title)")
+        await fetchNewRecipe()
+    }
+    func fetchNewRecipe() async {
+        Task {
+            self.recipe = try await fetchRandomRecipe()
+        }
+    }
 }
 
 struct MainView: View {
     @StateObject var model = MainViewModel()
     var body: some View {
         NavigationStack {
-            SwipableRecipeCard(recipe: $model.recipe)
+            SwipableRecipeCard(
+                recipe: $model.recipe,
+                onSwipeLeft: { Task { await model.discardRecipe() } },
+                onSwipeRight: { Task { await model.saveRecipe() } }
+            )
                 .offset(y: -50)
                 .navigationBarTitle("Snack Swipe", displayMode: .inline)  // Scroll View in Recipe card messes up the title, so this is fix. :/
                 .toolbar {
--- Recipe Swiper/Views/OnboardingView.swift
+++ Recipe Swiper/Views/OnboardingView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("This is the onboarding screen")
+                Button("Next") {
+                    UserDefaults.standard.set(false, forKey: "isOnboarding")
+                }
+            }
+                
+        }
     }
 }
 
