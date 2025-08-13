148a794989c3afae5dc8256a9ce4167d4071e8f7
Rapphy <36800640+rapphy243@users.noreply.github.com>
2025-08-13 01:59:29 -0500

    Add random quotes back to mainview

--- Recipe Swiper/Views/Components/RecipeCard/RecipeCard.swift
+++ Recipe Swiper/Views/Components/RecipeCard/RecipeCard.swift
@@ -10,97 +10,95 @@ import SwiftUI
 struct RecipeCard: View {
     @ObservedObject var recipe: Recipe
     var body: some View {
-        VStack {
-            Card {
-                VStack(spacing: 5) {
-                    Text(recipe.title)
-                        .font(.title)
-                        .bold()
-                        .multilineTextAlignment(.center)
-                        .padding(.bottom)
-                    Divider()
-                        .containerRelativeFrame(
-                            .horizontal,
-                            { width, _ in
-                                return width * 0.87
-                            }
-                        )
-                    VStack {
-                        HStack {
-                            AsyncImage(
-                                url: URL(
-                                    string:
-                                        recipe.image ?? ""
-                                )
-                            ) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(height: 295)
-                                    .containerRelativeFrame(
-                                        .horizontal,
-                                        { width, _ in
-                                            return width / 2
-                                        }
-                                    )
-                                    .clipped()
-                                    .cornerRadius(10)
-                            } placeholder: {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.gray.opacity(0.3))
-                                    .frame(height: 295)
-                                    .containerRelativeFrame(
-                                        .horizontal,
-                                        { width, _ in
-                                            return width / 2
-                                        }
-                                    )
-                            }
-                            VStack(alignment: .center) {
-                                RecipeCardDetails(recipe: recipe)
-                                    .containerRelativeFrame(
-                                        .horizontal,
-                                        { width, _ in
-                                            return width * 0.35
-                                        }
-                                    )
-                                Divider()
-                                    .containerRelativeFrame(
-                                        .horizontal,
-                                        { width, _ in
-                                            return width * 0.3
-                                        }
-                                    )
-
-                                ScrollView {
-                                    if let generatedSummary = recipe.generatedSummary {
-                                        Text(generatedSummary)
-                                            .font(.caption)
-                                    }
-                                    else {
-                                        Text(recipe.summary)
-                                            .font(.caption)
-                                    }
-                                }
-                                // TODO: Fix size to image wsize
-                                .containerRelativeFrame(
-                                    .vertical,
-                                    { height, _ in
-                                        return height * 0.26
-                                    }
-                                )
+        Card {
+            VStack(spacing: 5) {
+                Text(recipe.title)
+                    .font(.title)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom)
+                Divider()
+                    .containerRelativeFrame(
+                        .horizontal,
+                        { width, _ in
+                            return width * 0.87
+                        }
+                    )
+                VStack {
+                    HStack {
+                        AsyncImage(
+                            url: URL(
+                                string:
+                                    recipe.image ?? ""
+                            )
+                        ) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 295)
                                 .containerRelativeFrame(
                                     .horizontal,
                                     { width, _ in
-                                        return width * 0.31
+                                        return width / 2
                                     }
                                 )
-                            }
+                                .clipped()
+                                .cornerRadius(10)
+                        } placeholder: {
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.gray.opacity(0.3))
+                                .frame(height: 295)
+                                .containerRelativeFrame(
+                                    .horizontal,
+                                    { width, _ in
+                                        return width / 2
+                                    }
+                                )
                         }
-                        .padding(.top)
-                    }
-                }
+                        VStack(alignment: .center) {
+                            RecipeCardDetails(recipe: recipe)
+                                .containerRelativeFrame(
+                                    .horizontal,
+                                    { width, _ in
+                                        return width * 0.35
+                                    }
+                                )
+                            Divider()
+                                .containerRelativeFrame(
+                                    .horizontal,
+                                    { width, _ in
+                                        return width * 0.3
+                                    }
+                                )
 
+                            ScrollView {
+                                if let generatedSummary = recipe
+                                    .generatedSummary
+                                {
+                                    Text(generatedSummary)
+                                        .font(.caption)
+                                } else {
+                                    Text(recipe.summary)
+                                        .font(.caption)
+                                }
+                            }
+                            // TODO: Fix size to image wsize
+                            .containerRelativeFrame(
+                                .vertical,
+                                { height, _ in
+                                    return height * 0.26
+                                }
+                            )
+                            .containerRelativeFrame(
+                                .horizontal,
+                                { width, _ in
+                                    return width * 0.31
+                                }
+                            )
+                        }
+                    }
+                    .padding(.top)
+                }
             }
         }
     }
